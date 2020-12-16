5fe01f8b4a3b31d016ffd8dd83f017c202915738
CristiMunteanu <cristi.munteanu950@gmail.com>
2020-12-17 00:43:21 +0200

    Generate_pass_dict

--- main.py
+++ main.py
@@ -119,4 +119,66 @@ def gen_word_lengths(total_length):
     while current_len < total_length and total_length - current_len > 3:
         total_length -= current_len
         yield current_len
-    yield total_length
+        yield total_length
+
+def generate_password_dict():
+    words_by_len = assign_in_buckets(filter_words_by_length(PasswordCharacteristics.dict_info[1]))
+
+    password = ''
+    length = random.randint(PasswordCharacteristics.minimum_length, PasswordCharacteristics.maximum_length)
+    special_characters_count = random.randint(PasswordCharacteristics.min_required_characters,
+                                              PasswordCharacteristics.max_required_characters)
+    other_characters_count = random.randint(0, special_characters_count - 1)
+    special_characters_count -= other_characters_count
+
+    words_total_length = length - special_characters_count
+
+    selected_words = []
+
+    for word_len in gen_word_lengths(words_total_length):
+        selected_words.append(random.choice(words_by_len[word_len]))
+
+    print(selected_words)
+    print(special_characters_count)
+    print(other_characters_count)
+    print(length)
+
+    start_with_word = True if other_characters_count == 0 else bool(random.randint(0, 1))
+    if not start_with_word:
+        other_characters_count -= 1
+        password += random.choice(string.ascii_uppercase)
+    else:
+        password += selected_words.pop(selected_words.index(random.choice(selected_words)))
+
+    while len(selected_words) > 0 or other_characters_count > 0 or special_characters_count > 0:
+        if len(selected_words) >= other_characters_count and len(selected_words) >= special_characters_count and \
+                len(selected_words) > 0:
+            password += selected_words.pop(selected_words.index(random.choice(selected_words)))
+        elif other_characters_count >= len(selected_words) and other_characters_count >= special_characters_count and \
+                other_characters_count > 0:
+            password += random.choice(string.ascii_letters)
+            other_characters_count -= 1
+        elif special_characters_count > 0:
+            password += random.choice(PasswordCharacteristics.required_character)
+            special_characters_count -= 1
+
+    return password
+
+
+
+def start_script() -> object:
+    random.seed()
+
+    check_use_dict_option()
+    check_help_option()
+
+    if PasswordCharacteristics.dict_info[0] is False:
+        password = generate_password()
+    else:
+        password = generate_password_dict()
+
+    print(password)
+
+
+if __name__ == '_main_':
+    start_script()
